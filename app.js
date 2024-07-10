dc9c8c81ed2b51901c98d6a1f3b524efd7bdd0aa
keyur1411 <keyur14113@gmail.com>
2024-07-10 18:50:44 +0530

    fixed moogose problem

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 // connect to mongodb & listen for requests
 const dbURI = "mongodb+srv://keyur:keyur@keyur.ljjxolk.mongodb.net/key?retryWrites=true&w=majority&appName=keyur";
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(dbURI)
   .then(result => app.listen(3000))
   .catch(err => console.log(err));
 
